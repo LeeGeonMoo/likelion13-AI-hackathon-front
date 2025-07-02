fbd2648d971336105db4c81b57ffcce4ec5777b0
moo <lgmoo2002@gmail.com>
2025-07-02 23:53:03 +0900

    250702 project setting

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import HomePage from "./routes/HomePage";
+import CreateImage_1 from "./routes/CreateImage_1";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="template-container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create-image-1" element={<CreateImage_1 />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/routes/CreateImage_1.jsx
@@ -0,0 +1,5 @@
+const CreateImage_1 = () => {
+  return <h1>CreateImage_1</h1>;
+};
+
+export default CreateImage_1;
