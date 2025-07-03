4a38c595d0992f8e14e54da94a974fdea326462b
moo <lgmoo2002@gmail.com>
2025-07-03 22:07:38 +0900

    homepage 거의 완성성

--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import ChallengeListSection from "../components/challenge/ChallengeListSection";
 import HeaderSection from "../components/Header/HeaderSection";
 import UserProfileSection from "../components/User/UserProfileSection";
+import "../homepage_prompteer_style.css";
 
 export default function Index() {
   return (
     <>
       {/* 상단 고정 헤더 */}
-      <header className="flex items-center justify-between bg-white border-b border-[#e9ecef] px-6 py-4">
-        <h1 className="font-bold text-xl text-[#212529] [font-family:'Noto_Sans_KR-Bold',Helvetica]">
-          Prompteer
-        </h1>
-        <div className="flex-1" />
+      <header className="header">
+        <div className="frame">
+          <h3>Prompteer</h3>
+        </div>
       </header>
 
-      <main className="flex flex-col min-h-screen bg-[#f8f9fa] pt-4">
+      <main className="homepage-prompteer">
         <div className="flex flex-col items-center px-24 pt-0 pb-[396px]">
           <div className="flex flex-col max-w-[1248px] w-full items-start gap-5 p-6 relative">
             <HeaderSection />
